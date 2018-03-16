b60ec7f30a96fe4492ac8d6f8b8cb93e7a63b6eb
dvlbamasa <dvlbamasa@gmail.com>
2018-03-16 17:49:08 +0800

    Added new files

---
+++ Main.java
@@ -0,0 +1,14 @@
+import java.io.File;
+
+public class Main {
+
+     public static void main(String[] args) {
+		try {
+			FileParser.validateFile(args[0]);
+	  		new RandomizerApp2(new File(args[0])); 
+		} catch (InvalidFileInputException | ArrayIndexOutOfBoundsException e) {
+			System.out.println(e);
+		}
+    }
+}
+
